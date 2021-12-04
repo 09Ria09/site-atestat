99edc7ff705d32d57e785c2ed4a914d6866119d4
Rares Iordan <raresiordan09@gmail.com>
2021-12-04 18:39:29 +0200

    another two

--- src/js/NavBar.js
+++ src/js/NavBar.js
@@ -7,15 +7,10 @@ function NavBar(){
             <NavItem to={'1'}/>
             <NavItem to={'2'}/>
             <NavItem to={'3'}/>
-            <NavItem to={'4'}/>
-            <NavItem to={'5'}/>
-            <NavItem to={'6'}/>
-            <NavItem to={'7'}/>
-            <NavItem to={'8'}/>
             <NavItem to={'9'}/>
-            <NavItem to={'10'}/>
-            <NavItem to={'11'}/>
-            <NavItem to={'12'}/>
+            <NavItem to={'5'}/>
+            <NavItem to={'8'}/>
+            <NavItem to={'6'}/>
         </div>
     )
 }
--- src/js/Screen.js
+++ src/js/Screen.js
@@ -14,13 +14,15 @@ function Screen(props){
                         style={{
                             transform: `scale(${Math.min(1, percentage)})`
                         }}
-                    ><p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
-                    <img src={props.bg+'.png'} style={{
+                    >
+                        <h2>{props.title}</h2>
+                        <p>{props.children}</p></div>
+                    <img src={props.pic+'.png'} style={{
                         position: "absolute",
-                        right: 80/(-percentage)+15,
+                        right: -(1-Math.min(1,percentage))*500,
                         bottom: "0",
-                        width: 400,
-                        height: "auto"
+                        width: "auto",
+                        height: "70vh"
                     }}/>
                 </div>
             )}/>
